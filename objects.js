c7eb856e3bf6621f4d3854d74a1b9e574afa6103
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-18 23:35:42 +0200

    third

--- objects.js
+++ objects.js
@@ -2,6 +2,14 @@ let myFancyObject = {
     name: "Fatih Sultan Mehmet",  // die Reihenfolge der "keys" ist völlig egal
     age: 30,
     height: 1.73,
+    name1: function(){
+        console.log(123);
+        return 10
+    }
 }
 
-console.log(myFancyObject["height"]); 
+console.log(myFancyObject.name1()); 
+
+
+
+
