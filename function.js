ba59ac19405e6906b31afdfbdc77d3d5a062765a
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-18 22:57:31 +0200

    with without

--- function.js
+++ function.js
@@ -1,10 +1,20 @@
-
+let discount = 50;
+let price = 100;
  
-logCalculatedPrice(50, 500, 1.19);  // 50 = discount, 500 = price 
 
+//logCalculatedPrice
+logCalculatedPrice(50, 500, 1.19);  // 50 = discount, 500 = price 
 logCalculatedPrice(50, 400, 1.19);
 
+// with Parameter
 function logCalculatedPrice(discount, price, tax){  
     console.log((price - discount)* tax) ;  
+}
+// without Parameter
+// logCalculatedPrice1
+logCalculatedPrice1();
+
+function logCalculatedPrice1(){
+    console.log((price - discount)* 1.19);
 }
 
