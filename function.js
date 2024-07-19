fc9df09f8fa53c7ceaf6433c469459ed514df115
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-19 21:43:31 +0200

    english

--- function.js
+++ function.js
@@ -4,9 +4,8 @@ console.log(logCalculatedPrice(50, 500,));
 function logCalculatedPrice(price, discount) {
     let value = (discount - price) * 1.19;
 
-    console.log("vor return");  // hier wird das Wert wieder zurück gegeben
     return value;       // return-Wert ist das was zurückgegeben wird // return value is what is returned
-    console.log("nach return"); // ab dem return ist auch die zurück Übermittelung abgeschlossen   
+    
 }
 
 
