53206ac8179422e6956411d5046f24a1dfc4eb4f
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-19 23:35:25 +0200

    available

--- dom_manupulation.js
+++ dom_manupulation.js
@@ -6,5 +6,6 @@ title.innerHTML = "Bayrische Motoren Werke";
 
 
 document.getElementById('test_div').innerHTML = '<p>Test</p>';
-document.getElementById('test_div').classList.add('green_bg');
-document.getElementById('test_div').classList.remove('green_bg');
+
+
+document.getElementById('test_dive').classList.toggle('grenn_bg');
