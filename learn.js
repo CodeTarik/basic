498b962e6ff16ded4981bbf7f9fbf85ddbe40919
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-19 19:22:34 +0200

    returned

---
+++ learn.js
@@ -0,0 +1,16 @@
+
+//Aufgabe 01 - Variante No.1
+function fullName(name, surname){
+    return name + '' + surname;
+}
+
+console.log(fullName('Max', ' Mustermann'));
+
+function fullName1(name, surname) {
+    let full = name + '' + surname;
+    console.log(full);
+    return full;
+}
+
+fullName1('Max', ' Mustermann');
+
