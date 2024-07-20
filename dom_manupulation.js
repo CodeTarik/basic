b3426febb4862d56f95fe5f7319eca0b801b80e0
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-20 14:24:31 +0200

    onclick - myFunction

--- dom_manupulation.js
+++ dom_manupulation.js
@@ -1,10 +1,5 @@
 let title = document.getElementById('website_title');
 
-console.log(title.innerHTML);
-
-title.innerHTML = "Bayrische Motoren Werke";
-
-
 document.getElementById('test_div').innerHTML = '<p>Test</p>';
 
 
@@ -13,7 +8,12 @@ document.getElementById('test_div').classList.add('green_bg');
 document.getElementById('test_div').classList.remove('green_bg');
 document.getElementById('test_div').classList.toggle('grenn_bg');
 
-//document.getElementById('test_input').value = 123123;
+document.getElementById('test_input').value = 123123;
 //document.getElementById('test_input').setAttribute('value', 123);
 
 console.log(document.getElementById('test_input').value = 123123);
+
+function myFunction(){
+document.getElementById("myH1").setAttribute("class", "democlass");
+
+}
