220a8917e9a7e68ba970b89072fc52fb9ee28a85
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-20 15:25:15 +0200

    who is here the master

--- setAttribute.js
+++ setAttribute.js
@@ -1,3 +1,7 @@
 function myFunction(){
     document.getElementById("exercise").setAttribute("class", "sice");
+
 }
+
+//const element = document.getElementById("exercise");
+//document.getElementById("")
