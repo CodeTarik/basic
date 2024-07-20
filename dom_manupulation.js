26c2f8a18c7d76f8ac440ea91cbaf83243367e56
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-20 13:59:26 +0200

    setAttribute

--- dom_manupulation.js
+++ dom_manupulation.js
@@ -11,4 +11,6 @@ document.getElementById('test_div').innerHTML = '<p>Test</p>';
 
 document.getElementById('test_div').classList.add('green_bg');
 document.getElementById('test_div').classList.remove('green_bg');
-document.getElementById('test_dive').classList.toggle('grenn_bg');
+document.getElementById('test_div').classList.toggle('grenn_bg');
+
+document.getElementById('test_input').value = 123123;
