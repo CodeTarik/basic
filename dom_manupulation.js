68c6f242ec6e81292c52cef501b66152b7f92d18
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-20 22:11:09 +0200

    addEventListener

--- dom_manupulation.js
+++ dom_manupulation.js
@@ -1,36 +1,18 @@
-let title = document.getElementById('website_title');
 
-document.getElementById('test_div').innerHTML = '<p>Test</p>';
+//document.getElementById("our_p_tag").addEventListener("click", logger);
+
+document.addEventListener("DOMContentLoaded", function(){
+    const pTag = document.getElementById('our_p_tag');
+    if(pTag){
+        pTag.addEventListener("click", logger);
+    } else{
+        console.error("ID 'our_p_tag' existiert nicht!")
+    }
+});
 
 
 
-document.getElementById('test_div').classList.add('green_bg');
-document.getElementById('test_div').classList.remove('green_bg');
-document.getElementById('test_div').classList.toggle('grenn_bg');
-
-document.getElementById('test_input').value = 123123;
-//document.getElementById('test_input').setAttribute('value', 123);
-
-console.log(document.getElementById('test_input').value = 123123);
-
-function myFunction(){
-document.getElementById("myH1").setAttribute("class", "democlass");
-
+function logger(){
+    console.log(55);
 }
 
-
-function toggleDNone(id){
-    document.getElementById(id).classList.toggle('d-none');
-}
-
-
-function myCars(){
-    var x = document.getElementById("mySelect").value;
-    document.getElementById("brands").innerHTML = "Du hast " + x + " ausgewählt";
-}
-
-
-function myLogout() {
-    var inputValue = document.getElementById('buzz').value;
-    console.log("Input Value: " + inputValue);
-}
